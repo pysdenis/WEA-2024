0471a5b332da4b07cfa17eecccb8c882b942c612
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2024-03-29 01:07:27 +0100

    Update CORS header in index.php

--- backend/index.php
+++ backend/index.php
@@ -6,5 +6,5 @@ $articles = [
 ];
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *"); // Povolíme CORS pro všechny domény, pro produkční nasazení je lepší specifikovat povolené domény
 echo json_encode($articles);
