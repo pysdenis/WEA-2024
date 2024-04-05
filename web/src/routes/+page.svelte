3e559e0bb047e6bf19ad9cd28180de7da5c66e77
pysdenis <d.pys.st@spseiostrava.cz>
2024-04-05 12:18:00 +0200

    gzdgfzgzdf

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <h1 class="bg-slate-700 text-white">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev" class="leading-5 text-red-500">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://kit.svelte.dev" class="leading-5 text-red-500">kit.sbadfibdsfguidsfiguvelte.dev</a> to read the documentation</p>
 {#each articles as article}
 	<li>
 		<h2>{article.title}</h2>
