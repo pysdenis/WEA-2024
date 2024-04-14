b54a116b550580158883600a9bf844b2d8a04412
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2024-04-15 00:07:39 +0200

    @ Update Svelte routing and pageContent variable

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -2,16 +2,13 @@
 	import "$lib/assets/css/main.css";
 	import logo from "$lib/assets/images/logo.png";
 
-	let pageTitle = "Random Page";
-	let pageContent = "This is a test page.";
+	let pageTitle = "Random Page layout";
 </script>
 
 <main class="container mx-auto">
 	<h1 class="text-4xl font-bold bg-red-500">{pageTitle}</h1>
-	<p class="text-lg">{pageContent}</p>
-	TODO: Svelte routing
+	//TODO: https://kit.svelte.dev/docs/routing
 	<img src={logo} alt="Logo" />
-	<a href="/test">test</a>
 	<slot></slot>
 </main>
 
