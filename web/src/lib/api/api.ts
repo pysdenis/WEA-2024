2499da6b412ec6e80b16c5f8ce903d7a065d8bca
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2024-04-15 23:25:58 +0200

    + Update API functions in api.ts and add TypeScript types

--- web/src/lib/api/api.ts
+++ web/src/lib/api/api.ts
@@ -1,14 +1,12 @@
-import type Article from "../types/Article";
-
 const BASE_URL = 'http://localhost:8000'; // upravit a vymyslet, tak aby fungovalo lokálně i pak na serveru
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export async function fetchData(endpoint: string): Promise<Record<string, Article>>{
+export async function fetchData(endpoint: string): Promise<Record<string, unknown>>{
 	const response = await fetch(`${BASE_URL}`); // dopsat '/api/${endpoint}' jak je níže
 	return await response.json();
 }
 
-export async function postData(endpoint: string, data: Record<string, unknown>): Promise<Record<string, Article>>{
+export async function postData(endpoint: string, data: Record<string, unknown>): Promise<Record<string, unknown>>{
 	const response = await fetch(`${BASE_URL}/api/${endpoint}`, {
 		method: 'POST',
 		headers: {
