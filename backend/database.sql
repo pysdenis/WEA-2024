79d084814b5b57bda25588247868e6e20dd3e44c
pysdenis <d.pys.st@spseiostrava.cz>
2024-05-30 01:46:40 +0200

    updated sql dump for creating tables

---
+++ backend/database.sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Počítač: 127.0.0.1
+-- Vytvořeno: Čtv 30. kvě 2024, 01:43
+-- Verze serveru: 10.4.28-MariaDB
+-- Verze PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databáze: `id22133666_wea`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `title` varchar(150) NOT NULL,
+  `createdAt` date DEFAULT NULL,
+  `publishedAt` date DEFAULT NULL,
+  `categoryId` int(11) DEFAULT NULL,
+  `authorId` int(11) DEFAULT NULL,
+  `image` varchar(300) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `perex` varchar(200) DEFAULT NULL,
+  `urlSlug` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `authors`
+--
+
+CREATE TABLE `authors` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(30) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `phoneNumber` varchar(15) NOT NULL,
+  `content` text NOT NULL,
+  `image` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `content` text DEFAULT NULL,
+  `image` varchar(300) DEFAULT NULL,
+  `inMenu` tinyint(1) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `urlSlug` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabulky `users_admin`
+--
+
+CREATE TABLE `users_admin` (
+  `id` int(11) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `loginName` varchar(100) NOT NULL,
+  `loginPassword` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexy pro exportované tabulky
+--
+
+--
+-- Indexy pro tabulku `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_title` (`title`),
+  ADD UNIQUE KEY `unique_urlSlug` (`urlSlug`),
+  ADD KEY `categoryId` (`categoryId`),
+  ADD KEY `authorId` (`authorId`);
+
+--
+-- Indexy pro tabulku `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_email` (`email`);
+
+--
+-- Indexy pro tabulku `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_urlSlug` (`urlSlug`),
+  ADD UNIQUE KEY `unique_title` (`name`);
+
+--
+-- Indexy pro tabulku `users_admin`
+--
+ALTER TABLE `users_admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_email` (`email`);
+
+--
+-- AUTO_INCREMENT pro tabulky
+--
+
+--
+-- AUTO_INCREMENT pro tabulku `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pro tabulku `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pro tabulku `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pro tabulku `users_admin`
+--
+ALTER TABLE `users_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `articles_ibfk_2` FOREIGN KEY (`authorId`) REFERENCES `authors` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
