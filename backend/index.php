537685c7b43d7431ab559abeb4c9554a8649615a
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2024-06-03 01:10:45 +0200

    chore: Update Access-Control-Allow-Origin header to allow multiple origins

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://thecap.thatrichard.space, http://localhost:5173");
+header("Access-Control-Allow-Origin: https://thecap.thatrichard.space"); // http://localhost:5173
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
