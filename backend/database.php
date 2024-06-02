b6b147de3e928829f0e2eb671d2a13041c1bba87
Denis Pyš <128090772+pysdenis@users.noreply.github.com>
2024-06-02 22:33:44 +0200

    chore: Update database credentials for production environment

--- backend/database.php
+++ backend/database.php
@@ -2,8 +2,8 @@
 class Database {
 	private $connection;
 	private $servername = "localhost";
-	private $username = "root"; //fastest
-	private $password = ""; //rychleAzbesile5@
+	private $username = "id22260045_denis";
+	private $password = "rychleAzbesile5555@";
 	private $dbname = "id22133666_wea";
 
 	public function getConnection() {
